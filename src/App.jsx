e302552c4f11c0d69f38d8afc519b710857efaa1
Nikunj Singh <nikunj.singh@live.com>
2025-03-11 20:33:16 -0400

    created home and favorites pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
  
   return (
      <>
-     <MovieCard movie={{title: "niks film", release_date: "2025"}} />
+     <Home />
      </>
   )
 }
