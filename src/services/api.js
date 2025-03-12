ec7aa84c609f8a3e4f89b9b3868881858d4a0ecb
Nikunj Singh <nikunj.singh@live.com>
2025-03-12 12:12:15 -0400

    got movies from api

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,17 @@ const API_KEY = "ce3347d3ae4ad7088995dd0bceeaa818";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-}
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
