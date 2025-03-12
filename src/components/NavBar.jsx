04fd3f57d7bf599f714b0e3307eb0af799c92105
Nikunj Singh <nikunj.singh@live.com>
2025-03-12 18:36:46 -0400

    few css and color stylying

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,11 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link to="/">Niks Movie DB</Link>
       </div>
       <div className="navbar-links">
-        <Link to="/" className="nav-link">Home</Link>
-        <Link to="/favorites" className="nav-link">Favorites</Link>
+        <Link to="/" className="nav-link">Trending Movies</Link>
+        <Link to="/favorites" className="nav-link">Favorite Movies</Link>
       </div>
     </nav>
   );
