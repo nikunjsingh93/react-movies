447048a485e7c51e94409236e2a46aa26b3b34a8
Nikunj Singh <nikunj.singh@live.com>
2025-03-12 17:13:23 -0400

    favorites page working

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,29 @@
-import "../css/Favorites.css"
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 function Favorites() {
+  const { favorites } = useMovieContext();
 
-    return <div className="favorites-empty"> 
-     <h2> No Favorite Movie Yet </h2>
-     <p>Start adding movies to your favorites</p>
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorite Movies</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="favorites-empty">
+      <h2> No Favorite Movie Yet </h2>
+      <p>Start adding movies to your favorites</p>
     </div>
-
+  );
 }
 
-export default Favorites
+export default Favorites;
