809fb79c9ad6402ba8d9f85591732ab2b1896c10
Nikunj Singh <nikunj.singh@live.com>
2025-03-11 22:16:57 -0400

    added prettier and state variable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 function Home() {
-    const movies = [
-        { id: 1, title: "John Wick", release_date: "2020" },
-        { id: 2, title: "Terminator", release_date: "1999" },
-        { id: 3, title: "The Matrix", release_date: "2004" },
-    ];
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const handleSearch = () => {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1999" },
+    { id: 3, title: "The Matrix", release_date: "2004" },
+  ];
 
-    }
+  const handleSearch = () => {};
 
-
-    return <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search Movies..." className="search-input"></input>
-            <button type="submit" className="search-button">Search</button>
-        </form>
-        <div className="movies-grid">
-            {movies.map(movie => <MovieCard movie={movie} key={movie.id}></MovieCard>)}
-        </div>
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search Movies..."
+          className="search-input"
+          value={searchQuery}
+        ></input>
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
     </div>
+  );
 }
 
-export default Home
+export default Home;
