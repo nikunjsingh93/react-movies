f1e71f1d746d163a9f4bf6bdc019117db1ff7b0b
Nikunj Singh <nikunj.singh@live.com>
2025-03-12 11:22:41 -0400

    added searvices folder to call api

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+const API_KEY = "ce3347d3ae4ad7088995dd0bceeaa818";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+}
