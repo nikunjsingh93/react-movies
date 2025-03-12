5001dee34a514862703bbdf6c5ba314bbec35054
Nikunj Singh <nikunj.singh@live.com>
2025-03-11 23:28:10 -0400

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
